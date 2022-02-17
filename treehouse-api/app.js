95b1076bfd74a733508c5ef3cc73f691aad585f2
Clayton William <clayton.calvin@gmail.com>
2022-02-16 21:52:18 -0500

    wrap it into a function

--- treehouse-api/app.js
+++ treehouse-api/app.js
@@ -1,28 +1,29 @@
 // simple way to look at user badge count and js points
 const https = require("https");
-const { type } = require("os");
-const userName = "cw10";
-const apiUrl = `https://teamtreehouse.com/${userName}.json`;
 
 const printMessage = (userName, badgeCount, points) =>
   `${userName} has ${badgeCount} total badge(s) and ${points} points in JavaScript.`;
 // connect to API url
-https.get(apiUrl, (res) => {
-  // console.dir(res.headers);
-  let d = "";
-  // data event
-  res.on("data", (data) => {
-    d += data;
-  });
+const getProfileInfo = (userName) => {
+  const apiUrl = `https://teamtreehouse.com/${userName}.json`;
+  https.get(apiUrl, (res) => {
+    // console.dir(res.headers);
+    let d = "";
+    // data event
+    res.on("data", (data) => {
+      d += data;
+    });
 
-  // end data event
-  res.on("end", () => {
-    const profile = JSON.parse(d);
-    const badges = profile.badges.length;
-    const jsPoints = profile.points.JavaScript;
-    const msg = printMessage(userName, badges, jsPoints);
-    console.log(msg);
+    // end data event
+    res.on("end", () => {
+      const profile = JSON.parse(d);
+      const badges = profile.badges.length;
+      const jsPoints = profile.points.JavaScript;
+      const msg = printMessage(userName, badges, jsPoints);
+      console.log(msg);
+    });
   });
-});
-// parse the data
-// print the data
+};
+
+const users = ["chalkers", "cw10"];
+users.forEach(getProfileInfo);
