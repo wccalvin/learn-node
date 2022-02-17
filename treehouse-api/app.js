e8ac791a22d16144b81da5dec3ab99dc30357bf0
Clayton William <clayton.calvin@gmail.com>
2022-02-16 21:22:59 -0500

    use data event to get the output of request

--- treehouse-api/app.js
+++ treehouse-api/app.js
@@ -1,5 +1,6 @@
 // simple way to look at user badge count and js points
 const https = require("https");
+const { type } = require("os");
 const userName = "cw10";
 const apiUrl = `https://teamtreehouse.com/${userName}.json`;
 
@@ -8,10 +9,18 @@ const printMessage = (userName, badgeCount, points) =>
 // connect to API url
 https.get(apiUrl, (res) => {
   console.log(`status: ${res.statusCode}`);
-  console.log("headers:");
-  console.dir(res.headers);
+  // console.log("headers:");
+  // console.dir(res.headers);
+  let d = "";
+  res.on("data", (data) => {
+    d += data;
+  });
+
+  res.on("end", () => {
+    console.log(d);
+    console.log(typeof d);
+  });
 });
-// read the data
 // parse the data
 // print the data
 
