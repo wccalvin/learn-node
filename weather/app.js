f1fcb5a7d3c3ef7b0d802ed9f4518add6d13facf
wccalvin <clayton.calvin@gmail.com>
2022-02-18 08:57:29 -0500

    fix: allow location with space on it

--- weather/app.js
+++ weather/app.js
@@ -1,20 +1,24 @@
-// use the https://openweathermap.org/ api to get the current weather
-// accept arguments
+/*
+use the https://openweathermap.org/ api to get the current weather for the given
+location
+*/
 
 const location = process.argv.slice(2);
 if (location.length === 0) {
   const scriptName = process.argv[1];
   console.error("Provide the location as 'city, country' or zipcode.");
-  console.log(`Example: node ${scriptName} London, UK`);
+  console.log(`Example: node ${scriptName} New York, US`);
   console.log(`Example: node ${scriptName} 10001`);
 }
 
 const processLocation = (location) => {
   let processedLocation = [];
+  // join the given string
+  location = location.join(" ");
+  location = location.split(",");
   location.forEach((loc) => {
-    loc.trim();
-    loc = loc.replace(/\,/g, "");
-    processedLocation.push(loc.toLowerCase());
+    const location = loc.trim();
+    processedLocation.push(location.toLowerCase());
   });
   return processedLocation.join(",");
 };
